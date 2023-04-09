6ad21b98e4a2172682a15b766c152445f9fe1b5f
Shabnoor Parveen <shabnoorparveen1999@gmail.com>
2023-04-09 22:33:02 +0530

    commit

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import "./comments.scss";
+import { AuthContext } from "../../context/authContext";
+
+const Comments = () => {
+  const { currentUser } = useContext(AuthContext);
+  //Temporary
+  const comments = [
+    {
+      id: 1,
+      desc: "Think about what people are doing on Facebook today. They’re keeping up with their friends and family, but they’re also building an image and identity for themselves, which in a sense is their brand. They’re connecting with the audience that they want to connect to. It’s almost a disadvantage if you’re not on it now.",
+      name: "Mudit Sharma",
+      userId: 1,
+      profilePicture:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      desc: "Social media is not a media. The key is to listen, engage, and build relationships.",
+      name: "Shabnoor Parveen",
+      userId: 2,
+      profilePicture:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+  ];
+  return (
+    <div className="comments">
+      <div className="write">
+        <img src={currentUser.profilePic} alt="" />
+        <input type="text" placeholder="write a comment" />
+        <button>Send</button>
+      </div>
+      {comments.map((comment) => (
+        <div className="comment">
+          <img src={comment.profilePicture} alt="" />
+          <div className="info">
+            <span>{comment.name}</span>
+            <p>{comment.desc}</p>
+          </div>
+          <span className="date">1 hour ago</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Comments;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
+
+const Login = () => {
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    login();
+  };
+
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          {/* <h4>Talk-a-Tive</h4> */}
+          <p>
+          <u>Talk-a-Tive</u> : Your gateway to the world of social media!
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
